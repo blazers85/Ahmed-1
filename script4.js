d67fd72e5723fbfedade2ce0767f115a14e6f6a4
blazers85 <ahmednw9@gmail.com>
2018-07-03 16:57:55 -0700

    my react app

---
+++ script4.js
@@ -0,0 +1,17 @@
+fucntion Contact(name, number, email) {
+  this.name = name;
+  this.number = number;
+  this. email = email;
+}
+
+let tyrionLanister = new Contact('Tyrion Lanister', '333-3312', 'idrinkandiknowthing')
+let jonsnow = new Contact('Jon Snow', '444-4444', 'iknownothing@got.com');
+let cirsi = new Contact('Jamie Lanister','666-6666','helpineedahand@got.com');
+console.log();
+
+
+
+
+
+
+
