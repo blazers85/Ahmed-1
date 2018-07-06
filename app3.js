7ef29ac4764312989d90a89426f0f159f0af8252
blazers85 <ahmednw9@gmail.com>
2018-07-05 17:19:46 -0700

    my assignment

---
+++ app3.js
@@ -0,0 +1,5 @@
+let NewContactForm = document.getElementByid('new-contact-form');
+let nameInput = document.getElementByid('name-input');
+let numberInput = document.getElementByid('number-input');
+let emailInput = document.getElementByid('email-input');
+let contactUl = document.getElementByid('contact-ul');
